1e820113ee6aeaf43f32e65530562fbc8fd844c5
ablasco86 <ablasco86@gmail.com>
2014-02-18 14:22:36 +0100

    Create cudavectors.h

---
+++ cudavectors.h
@@ -0,0 +1,15 @@
+/*
+ * cudavectors.h
+ *
+ *  Created on: Jan 10, 2013
+ *      Author: dbd
+ */
+
+#ifndef CUDAVECTORS_H_
+#define CUDAVECTORS_H_
+
+#include <cuda_runtime.h>
+
+
+
+#endif /* CUDAVECTORS_H_ */
